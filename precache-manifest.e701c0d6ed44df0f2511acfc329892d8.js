f38f4dfe11c61410f33b17ff77fec4e563c34ec0
Harry Bowyer <harry.h.bowyer@gmail.com>
2019-09-29 01:27:39 -0500

    initial commit

---
+++ precache-manifest.e701c0d6ed44df0f2511acfc329892d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3cfb5a44755b226efced20a9076ac11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b1cf85af946a31d3651",
+    "url": "/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "c8d8adb9601e61e66b59",
+    "url": "/static/css/main.2c9e5296.chunk.css"
+  },
+  {
+    "revision": "1b1cf85af946a31d3651",
+    "url": "/static/js/2.c7b08438.chunk.js"
+  },
+  {
+    "revision": "c8d8adb9601e61e66b59",
+    "url": "/static/js/main.cb17be43.chunk.js"
+  },
+  {
+    "revision": "fc7bdb9cb2dacc9a0b3e",
+    "url": "/static/js/runtime-main.477f0c69.js"
+  }
+]);
