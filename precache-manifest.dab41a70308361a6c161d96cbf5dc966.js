1778bc42d1a3dcad9b36b1c425d69ae8ed056d74
Harry Bowyer <harry.h.bowyer@gmail.com>
2019-10-24 21:18:33 -0500

    automated script

--- precache-manifest.dab41a70308361a6c161d96cbf5dc966.js
+++ precache-manifest.dab41a70308361a6c161d96cbf5dc966.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c4e3fe6f42fb0d1a604df1a1af24060f",
+    "revision": "d4daa99826df041768cd7047e107e0e6",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.aa7fcd32.chunk.css"
   },
   {
-    "revision": "325e9ca0d489c62aa8c1",
-    "url": "/static/css/main.cc56d0bd.chunk.css"
+    "revision": "fd2e2f056d9c81c2f2c3",
+    "url": "/static/css/main.8a5f580f.chunk.css"
   },
   {
     "revision": "7a77cd8839863d9a1ee4",
     "url": "/static/js/2.1a8e7005.chunk.js"
   },
   {
-    "revision": "325e9ca0d489c62aa8c1",
-    "url": "/static/js/main.81acf3ea.chunk.js"
+    "revision": "fd2e2f056d9c81c2f2c3",
+    "url": "/static/js/main.ec94036b.chunk.js"
   },
   {
     "revision": "ba0c7f773a2d17725417",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44de347453f030becc24d4b388ba4445.js"
+  "/precache-manifest.dab41a70308361a6c161d96cbf5dc966.js"
 );
 
 self.addEventListener('message', (event) => {
